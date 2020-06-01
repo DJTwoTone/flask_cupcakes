7528a1790355c63b411ecf6a3e7f07b2a6d48772
DJTwoTone <DJTwoTone@gmail.com>
2020-06-01 17:11:32 +0900

    setup done-ish

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = 'yummyyummyinmytummy'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
